1378e29524b97d20b2fdceee3f1783cd1d982918
MoeBai <51248296+MoeBai@users.noreply.github.com>
2024-06-09 20:55:52 +0800

    Update the domain

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
+  "docker.proxy.vme60.link": "https://registry-1.docker.io",
   "quay.libcuda.so": "https://quay.io",
   "gcr.libcuda.so": "https://gcr.io",
   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
   "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
+  "ghcr.proxy.vme60.link": "https://ghcr.io",
   "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
 };
 
